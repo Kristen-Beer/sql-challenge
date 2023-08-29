f0f6e15a3af6abaef257f1c894d5b5b3ccb04091
Kristen-Beer <Krismason56@gmail.com>
2023-08-28 21:33:18 -0700

    Started Analysis Query

---
+++ EmployeeSQL/Query_schema.sql
@@ -0,0 +1,32 @@
+-- Analysis Queries
+-- 1. List the employee number, last name, first name, sex, and salary of each employee
+SELECT employees.emp_no
+	,employees.last_name
+	,employees.first_name
+	,employees.sex
+	,employees.salary
+FROM employees
+INNER JOIN salaries ON
+	employees.emp_no=salaries.emp_no;
+
+
+-- 2. List the first name, last name, and hire date for the employees who were hired in 1986
+
+
+-- 3. Listh the manager of each department along with their department number, department name, employee number, last name, and first name.
+
+
+-- 4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+
+-- 5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+
+-- 6. List each employee in the Sales department, including their employee number, last name, and first name.
+
+
+-- 7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+-- 8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
