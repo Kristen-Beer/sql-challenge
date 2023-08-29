9313c6994d7d68ce0b11693aea6b43d6c12c6fec
Kristen-Beer <Krismason56@gmail.com>
2023-08-28 20:55:15 -0700

    added new schema and DBD png

--- EmployeeSQL/Table_schema.sql
+++ EmployeeSQL/Table_schema.sql
@@ -2,25 +2,24 @@
 
 -- Create table departments
 DROP TABLE departments
-CREATE TABLE departments (
+
+CREATE TABLE departments(
 		dept_no VARCHAR(10) 
-		,dept_name VARCHAR(30) NOT NULL
+		,dept_name TEXT NOT NULL
 		,PRIMARY KEY (dept_no)
 );
 
 select * from departments
 
--- Create table dept_emp
-DROP TABLE dept_emp
+--Create table titles
+DROP TABLE titles
 
-
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL
-	,dept_no VARCHAR(10) NOT NULL
-	,PRIMARY KEY (emp_no)
-	,FOREIGN KEY(dept_no) REFERENCES departments(dept_no)	
+CREATE TABLE titles(
+	title_id VARCHAR(10) NOT NULL
+	,title TEXT NOT NULL
+	,PRIMARY KEY (title_id)
 );
-select * from dept_emp
+select * from titles
 
 --Create table employees
 DROP TABLE employees
@@ -29,27 +28,40 @@ CREATE TABLE employees (
 	emp_no INT NOT NULL
 	,emp_title_id VARCHAR(10) NOT NULL 
 	,nbirth_date DATE NOT NULL
-	,first_name VARCHAR(30) NOT NULL
-	,last_name VARCHAR(30) NOT NULL
+	,first_name TEXT NOT NULL
+	,last_name TEXT NOT NULL
 	,sex VARCHAR(10) NOT NULL
 	,hire_date DATE NOT NULL
-	,PRIMARY KEY (emp_title_id)
+	,PRIMARY KEY (emp_no)
+	,FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 	
 );
 select * from employees
 
+-- Create table dept_emp
+DROP TABLE dept_emp
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL
+	,dept_no VARCHAR(10) NOT NULL
+	,PRIMARY KEY (emp_no, dept_no)
+	,FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	,FOREIGN KEY (dept_no) REFERENCES departments(dept_no)	
+);
+select * from dept_emp
 
 
 -- Create table dept_managers
 DROP TABLE dept_managers
 
-CREATE TABLE dept_managers (
+CREATE TABLE dept_manager(
 	dept_no VARCHAR(10) NOT NULL
 	,emp_no INT NOT NULL
+	,PRIMARY KEY (dept_no, emp_no)
 	,FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
 	,FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
-select * from dept_managers
+select * from dept_manager
 
 --Create table salaries
 DROP TABLE salaries
@@ -57,16 +69,9 @@ DROP TABLE salaries
 CREATE TABLE salaries (
 	emp_no INT NOT NULL
 	,salary INT NOT NULL
+	,PRIMARY KEY (emp_no, salary)
 	,FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
 select * from salaries
 
---Create table titles
-DROP TABLE titles
 
-CREATE TABLE titles (
-	emp_title_id VARCHAR(10) NOT NULL
-	,title VARCHAR(20) NOT NULL
-	,FOREIGN KEY(emp_title_iD) REFERENCES employees(emp_title_id)
-);
-select * from titles
